da459d56c2cff9ec60f3d5aea8a4cf43029d1f9a
Tomás Emmanuel Pereyra <tpgstdf@gmail.com>
2021-10-19 19:42:27 -0300

    archivos nuevos

--- Encapsulamiento/src/tests/PruebaPersona.java
+++ Encapsulamiento/src/tests/PruebaPersona.java
@@ -8,13 +8,19 @@ import dominio.Persona;
 
 /**
  *
- * @author Cintia
+ * @author Tomás Emmanuel Pereyra
  */
 public class PruebaPersona {
     public static void main(String[] args) {
         Persona persona1 = new Persona("Juan" , 5000.00, false);
+        System.out.println("persona1 nombre: " + persona1.getName());
         
         persona1.setName("Juan Carlos");
-        System.out.println(persona1.getName());
+        System.out.println("persona1 nombre con cambio: " + persona1.getName());
+        
+        System.out.println("persona1 sueldo: " + persona1.getSueldo());
+        System.out.println("persona1 eliminado: " + persona1.isEliminado());
+        
+        
     }
 }
